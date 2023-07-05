99932060dbd1a097ed651cc9026530d15654cc1d
Эрнисов Айдар <Aidar.ernisov01@gmail.com>
2023-07-06 02:11:57 +0600

    User Profile, Reset Password, Order Views

--- main/urls.py
+++ main/urls.py
@@ -4,13 +4,25 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     #Страница авторизации
+    path('', views.redirect_to_home, name="index"),
     path('accounts/login/', views.index, name="login"),
     path('doLogin/', views.doLogin, name="doLogin"),
     path('logout/', views.logout_page, name='logout_user'),
+
+    # Reset password
+    path("accounts/password_reset/", views.password_reset_request, name="password_reset"),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name="pages/user/password_reset_done.html"), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="pages/user/password_reset_form.html"), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name="pages/user/password_reset_complete.html"), name='password_reset_complete'),
+
+    path(r'profile/update/^', views.ProfileUpdateView.as_view(), name = 'update_profile'),
+    path(r'profile/update/^/change_password/', views.ChangePasswordView.as_view(), name = 'change_password'),
 
     #URL's for carriercompany
     path('carriercompany-home/', views.carriercompanypage, name="carriercompany"),
     #URL's for shippercompany
     path('shippercompany-home/', views.shippercompanypage, name="shippercompany"),
     path('shippercompany/create-order/', views.OrderCreateView.as_view(), name="createorder"),
+
+
 ]
--- main/views.py
+++ main/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from main.EmailBackEnd import EmailBackEnd
 from django.contrib import messages
-from django.contrib.auth.models import User
-from django.contrib import admin
 from django.contrib.auth.decorators import user_passes_test, login_required
 from django.views.decorators.cache import never_cache
 from django.core.cache import cache
@@ -17,6 +15,15 @@ from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse, reverse_lazy
 from main.forms import *
+from django.utils.http import urlsafe_base64_encode
+from django.conf import settings
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.template.loader import render_to_string
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse
+from django.contrib.auth.views import PasswordChangeView
+from django.db.models import Q
 
 # Create your views here.
 
@@ -30,9 +37,12 @@ def authentication_required(view_func):
     @login_required
     def wrapped_view(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect('login')  # Перенаправление на страницу входа
+            return redirect('login')  # Ты не вошел в аккаунт мал
         return view_func(request, *args, **kwargs)
     return wrapped_view
+
+def redirect_to_home(request):
+    return redirect('login')
 
 def index(request):
     return render(request, "login.html")
@@ -78,6 +88,69 @@ def carriercompanypage(request):
 def shippercompanypage(request):
     return render(request, "shippercompany/index.html")
 
+class ProfileView(View):
+    model = User
+    form_class = UpdateUserForm
+    success_url = reverse_lazy('update_profile')
+    
+
+class ProfileUpdateView(LoginRequiredMixin, SuccessMessageMixin, ProfileView, UpdateView):
+    login_url = "login_page"
+    success_message = "Данные успешно изменены"
+    template_name = 'carriercompany/pages/user/my_profile.html'
+    def get_object(self, queryset=None):
+        '''This method will load the object
+           that will be used to load the form
+           that will be edited'''
+        return self.request.user
+    
+class ChangePasswordView(SuccessMessageMixin, PasswordChangeView):
+    template_name = 'carriercompany/pages/user/change_password.html'
+    success_message = "Пароль успешно изменён"
+    success_url = reverse_lazy('update_profile')
+
+
+def password_reset_request(request):
+    # Установить учетные данные электронной почты
+    settings.EMAIL_HOST_USER='vkrsupp@gmail.com' 
+    settings.EMAIL_HOST_PASSWORD='gokuwvmbmhckgnfz' 
+    if request.method == "POST": # указывает на проверку, что метод запроса должен быть ПОСТ
+        # Создайте экземпляр формы UserPasswordResetForm с отправленными данными
+        password_reset_form = UserPasswordResetForm(request.POST) 
+        if password_reset_form.is_valid():
+            # Получить адрес электронной почты, введенный в форму
+            data = password_reset_form.cleaned_data['email']
+            print(data)
+            # Проверить, есть ли связанные пользователи с введенным адресом электронной почты
+            associated_users = CustomUser.objects.filter(Q(email=data))
+            if associated_users.exists():
+                for user in associated_users:
+                    # Подготовьте параметры электронной почты
+                    subject = "Запрос на сброс данных"
+                    email_template_name = "pages/user/password_reset_email.html"
+                    c = {
+                        "email":user.email,
+                        'domain':'http://127.0.0.1:8000',
+                        'site_name': 'Website',
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "user": user,
+                        'token': default_token_generator.make_token(user),
+                        'protocol': 'http',
+                        }
+                    email = render_to_string(email_template_name, c)
+                    
+                    try:
+                        # Отправьте электронное письмо для сброса пароля
+                        send_mail(subject, email, 'vkrsupp@gmail.com' , [user.email], fail_silently=False)
+                    except BadHeaderError:
+                        
+                        return HttpResponse('Invalid header found.')
+                    # Перенаправление на страницу сброса пароля
+                    return redirect ("/accounts/password_reset/done/")
+    # Создать новый экземпляр формы UserPasswordResetForm
+    password_reset_form = UserPasswordResetForm()
+    #Перейти на страницу сброса пароля с формой
+    return render(request=request, template_name="pages/user/password_reset.html", context={"password_reset_form":password_reset_form})
 
 #Order CRUD operations   
 class OrderView(View):
